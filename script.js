8d9732592a6ba326fce0156eb2a9a92306cae36b
vishal02011 <vishalmaurya.vm23@gmail.com>
2025-12-06 11:56:56 +0530

    changes in js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const USD_TO_INR = 84; 
-const quickCard = document.querySelector(".card-quick");
-const amountInput = quickCard.querySelector(".quick-input");
-const fromSelect = quickCard.querySelector(".from-cur");
-const toSelect = quickCard.querySelector(".to-cur");
-const outputBox = quickCard.querySelector(".quick-output");
-const previewBtn = quickCard.querySelector(".btn-secondary");
+const amountInput =document.querySelector(".quick-input");
+const fromSelect =document.querySelector(".from-cur");
+const toSelect =document.querySelector(".to-cur");
+const outputBox =document.querySelector(".quick-output");
 function convertQuick() {
     const amount = parseFloat(amountInput.value) || 0;
     if (fromSelect.value === "USD" && toSelect.value === "INR") {
@@ -15,7 +13,4 @@ function convertQuick() {
     }
 }
 amountInput.addEventListener("input", convertQuick);
-previewBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    convertQuick();
-});
+
